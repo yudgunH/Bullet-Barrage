a21037dfe5ee1ce04cac6fbf73d7fb56b79067e5
Nguyễn Duy Hưng <ndhung0901@gmail.com>
2024-07-09 00:23:07 +0700

    Update audio files and background file

--- Bullet-Barrage/main.cpp
+++ Bullet-Barrage/main.cpp
@@ -1,33 +1,102 @@
 ﻿#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-    // Khởi tạo SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+bool init(SDL_Window** window, SDL_Renderer** renderer) {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
         std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
-        return 1;
+        return false;
     }
 
-    // Tạo cửa sổ SDL
-    SDL_Window* window = SDL_CreateWindow("SDL2 Test Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
-    if (window == nullptr) {
+    *window = SDL_CreateWindow("Bullet Barrage Background",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (*window == NULL) {
         std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
-        SDL_Quit();
-        return 1;
+        return false;
     }
 
-    // Tạo renderer cho cửa sổ
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (renderer == nullptr) {
+    *renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED);
+    if (*renderer == NULL) {
         std::cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
+        return false;
     }
 
-    // Vòng lặp sự kiện chính
-    SDL_Event e;
+    int imgFlags = IMG_INIT_PNG;
+    if (!(IMG_Init(imgFlags) & imgFlags)) {
+        std::cerr << "SDL_image could not initialize! SDL_image Error: " << IMG_GetError() << std::endl;
+        return false;
+    }
+
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+        std::cerr << "SDL_mixer could not initialize! SDL_mixer Error: " << Mix_GetError() << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+SDL_Texture* loadTexture(const std::string& path, SDL_Renderer* renderer) {
+    SDL_Texture* newTexture = NULL;
+    SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+    if (loadedSurface == NULL) {
+        std::cerr << "Unable to load image " << path << "! SDL_image Error: " << IMG_GetError() << std::endl;
+    }
+    else {
+        newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+        if (newTexture == NULL) {
+            std::cerr << "Unable to create texture from " << path << "! SDL_Error: " << SDL_GetError() << std::endl;
+        }
+        SDL_FreeSurface(loadedSurface);
+    }
+    return newTexture;
+}
+
+void close(SDL_Window* window, SDL_Renderer* renderer, SDL_Texture* backgroundTexture, Mix_Music* backgroundMusic) {
+    SDL_DestroyTexture(backgroundTexture);
+    Mix_FreeMusic(backgroundMusic);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    Mix_CloseAudio();
+    IMG_Quit();
+    SDL_Quit();
+}
+
+int main(int argc, char* args[]) {
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
+
+    if (!init(&window, &renderer)) {
+        std::cerr << "Failed to initialize!" << std::endl;
+        return -1;
+    }
+
+    std::string imagePath = "..\\assets\\img\\mainBackGround.png";
+    std::cout << "Loading image from path: " << imagePath << std::endl;
+
+    SDL_Texture* backgroundTexture = loadTexture(imagePath, renderer);
+    if (backgroundTexture == NULL) {
+        std::cerr << "Failed to load background texture!" << std::endl;
+        close(window, renderer, backgroundTexture, nullptr);
+        return -1;
+    }
+
+    // Load and play background music
+    Mix_Music* backgroundMusic = Mix_LoadMUS("..\\assets\\audio\\Soundtrack - Bullet Barrage.mp3");
+    if (backgroundMusic == NULL) {
+        std::cerr << "Failed to load background music! SDL_mixer Error: " << Mix_GetError() << std::endl;
+        close(window, renderer, backgroundTexture, nullptr);
+        return -1;
+    }
+
+    Mix_PlayMusic(backgroundMusic, -1);
+
     bool quit = false;
+    SDL_Event e;
+
     while (!quit) {
         while (SDL_PollEvent(&e) != 0) {
             if (e.type == SDL_QUIT) {
@@ -35,18 +104,11 @@ int main(int argc, char* argv[]) {
             }
         }
 
-        // Màu nền (RGB)
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Màu trắng
         SDL_RenderClear(renderer);
-
-        // Cập nhật cửa sổ
+        SDL_RenderCopy(renderer, backgroundTexture, NULL, NULL);
         SDL_RenderPresent(renderer);
     }
 
-    // Hủy renderer và cửa sổ, thoát SDL
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
+    close(window, renderer, backgroundTexture, backgroundMusic);
     return 0;
 }
