f76b5a4543efa8fa4a81ab80e38696517db3d6bf
Nguyễn Duy Hưng <ndhung0901@gmail.com>
2024-07-08 20:12:38 +0700

    Set up sdl2.dll

---
+++ Bullet-Barrage/main.cpp
@@ -0,0 +1,52 @@
+﻿#include <SDL.h>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    // Khởi tạo SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    // Tạo cửa sổ SDL
+    SDL_Window* window = SDL_CreateWindow("SDL2 Test Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    // Tạo renderer cho cửa sổ
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr) {
+        std::cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Vòng lặp sự kiện chính
+    SDL_Event e;
+    bool quit = false;
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        // Màu nền (RGB)
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Màu trắng
+        SDL_RenderClear(renderer);
+
+        // Cập nhật cửa sổ
+        SDL_RenderPresent(renderer);
+    }
+
+    // Hủy renderer và cửa sổ, thoát SDL
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
